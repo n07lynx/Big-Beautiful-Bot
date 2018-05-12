928e0f2e82d3595626369d725d7a86cf0946f471
Unknown <zomanom@gmail.com>
2018-05-12 23:14:33 +1000

    Seperated code for weight comments

--- Scales.cs
+++ Scales.cs
@@ -18,21 +18,31 @@ namespace BigBeautifulBot
         {
             var weight = _Bot.Info.Weight;
             var response = string.Format(Resources.UseScales, weight) + '\n';
-
-            if (weight > 160) response += Resources.SelfWeightCommentGT150;
-            else if (weight > 150) response += Resources.SelfWeightComment150;
-            else if (weight > 140) response += Resources.SelfWeightComment140;
-            else if (weight > 130) response += string.Format(Resources.SelfWeightComment130, weight);
-            else if (weight > 120) response += Resources.SelfWeightComment120;
-            else if (weight > 110) response += Resources.SelfWeightComment110;
-            else if (weight > 100) response += string.Format(Resources.SelfWeightComment100, weight);
-            else if (weight > 90) response += Resources.SelfWeightComment90;
-            else if (weight > 80) response += Resources.SelfWeightComment80;
-            else if (weight > 70) response += Resources.SelfWeightComment70;
-            else if (weight > 60) response += Resources.SelfWeightComment60;
-            else response += Resources.SelfWeightCommentLT60;
-
+            response += GetWeightCommend(weight, true);
             await message.Channel.SendMessageAsync(response);
+        }
+
+        public static string GetWeightCommend(decimal weight, bool self)
+        {
+            if (self)
+            {
+                if (weight > 160) return Resources.SelfWeightCommentGT150;
+                else if (weight > 150) return Resources.SelfWeightComment150;
+                else if (weight > 140) return Resources.SelfWeightComment140;
+                else if (weight > 130) return string.Format(Resources.SelfWeightComment130, weight);
+                else if (weight > 120) return Resources.SelfWeightComment120;
+                else if (weight > 110) return Resources.SelfWeightComment110;
+                else if (weight > 100) return string.Format(Resources.SelfWeightComment100, weight);
+                else if (weight > 90) return Resources.SelfWeightComment90;
+                else if (weight > 80) return Resources.SelfWeightComment80;
+                else if (weight > 70) return Resources.SelfWeightComment70;
+                else if (weight > 60) return Resources.SelfWeightComment60;
+                else return Resources.SelfWeightCommentLT60;
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }

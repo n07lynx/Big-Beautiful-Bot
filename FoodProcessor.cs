990461a030dbc2484d0ec7d8c6660f069b1eef79
Unknown <zomanom@gmail.com>
2018-05-13 22:35:49 +1000

    Started on some appetite comments

--- FoodProcessor.cs
+++ FoodProcessor.cs
@@ -3,6 +3,7 @@ using Discord.WebSocket;
 using System.Collections.Generic;
 using BigBeautifulBot.Properties;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace BigBeautifulBot
 {
@@ -23,13 +24,32 @@ namespace BigBeautifulBot
             {
                 _Bot.Info.Weight += foodItem.WeightValue;
                 _Bot.Info.Appetite -= foodItem.WeightValue;
-                await message.Channel.SendMessageAsync(foodItem.BotComment);
-                //TODO: Comments as her appetite goes down
+
+                var responseText = foodItem.BotComment + '\n';
+                responseText += GetAppetiteComment(_Bot.Info.Appetite);
+                await message.Channel.SendMessageAsync(responseText);
             }
             else
             {
                 await message.Channel.SendMessageAsync(Resources.OverfedComment);
             }
+        }
+
+        private string GetAppetiteComment(decimal appetite)
+        {
+            if (appetite >= 11) return Resources.AppetiteCommentHungry;
+            else if (appetite >= 10) return string.Format(Resources.AppetiteComment10, Program.GetRandomElement(Definitions.Keys.ToArray()));
+            else if (appetite >= 9) return Resources.AppetiteComment9;
+            else if (appetite >= 8) return Resources.AppetiteComment8;
+            else if (appetite >= 7) return Resources.AppetiteComment7;
+            else if (appetite >= 6) return Resources.AppetiteComment6;
+            else if (appetite >= 5) return string.Format(Resources.AppetiteComment5, Program.GetRandomElement(Definitions.Keys.ToArray()));
+            else if (appetite >= 4) return Resources.AppetiteComment4;
+            else if (appetite >= 3) return Resources.AppetiteComment3;
+            else if (appetite >= 2) return Resources.AppetiteComment2;
+            else if (appetite >= 1) return Resources.AppetiteComment1;
+            else if (appetite >= 0) return Resources.AppetiteComment0;
+            else return Resources.AppetiteCommentFull;
         }
 
         public static readonly Dictionary<string, FoodInfo> Definitions = new Dictionary<string, FoodInfo>

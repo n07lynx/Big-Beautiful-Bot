c4607de41af0fcaf34b2ce99692801ae7915279b
Laura <zomanom@gmail.com>
2018-07-15 23:41:46 +1000

    Two projects co-existing

---
+++ Client/Client.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+
+namespace BigBeautifulBot.Client
+{
+    public class Program
+    {
+        static void Main(string[] args) => MainAsync(args).GetAwaiter().GetResult();
+        static async Task MainAsync(string[] args)
+        {
+
+        }
+    }
+}

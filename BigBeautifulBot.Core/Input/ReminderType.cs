99a150cecb07011081dd016c0444e2d2ecfeab15
n07lynx <42081220+n07lynx@users.noreply.github.com>
2018-08-04 03:50:34 +0000

    Added wrapper datatype for Reminder
    Added type ReminderType which wraps a string, Datetime and UserIdentity into a single ValueType

---
+++ BigBeautifulBot.Core/Input/ReminderType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SQLite;
+
+namespace BigBeautifulBot.Input
+{
+    public struct ReminderType
+    {
+        public UserIdentity User { get; }
+        public DateTime Time { get; }
+        public string RemText { get; }
+
+        /// <summary>
+        /// Returns Data saved in ReminderType object
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"New Reminder: ID: {this.User.UserName}, DATETIME: {this.Time.ToString()}, Text: {this.RemText}";
+        }
+
+        public ReminderType(string user, string datetimeISO, string remText)
+        {
+            User = new UserIdentity(user);
+            Time = DateTime.Parse(datetimeISO);
+            RemText = remText;
+        }
+    }
+}
